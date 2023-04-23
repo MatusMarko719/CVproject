b3964e1790efd29da670e825310ebefcca0b3910
MatusMarko719 <mmarko.matus@gmail.com>
2023-04-23 20:56:12 +0200

    enter CV

--- 01-starting-project/src/components/PostHeader.js
+++ 01-starting-project/src/components/PostHeader.js
@@ -2,6 +2,7 @@ import React, { useRef, useEffect } from "react";
 import foto2 from "../foto/foto2.jpg";
 import classes from "../components/PostHeader.module.css";
 import git from "../foto/git.png";
+import zivotopis from "../foto/CV.pdf";
 
 const PostHeader = () => {
   return (
@@ -22,7 +23,7 @@ const PostHeader = () => {
         </a>
         <button className={classes.button}>
           <a
-            href="https://ozcharbel.sk/Vyhlasenie_o_poukazani_podielu_zaplatenej_dane_z_prijmov_fyzickej_osoby.pdf"
+            href={zivotopis}
             target="blank"
           >
             Zobraziť CV (.pdf)
